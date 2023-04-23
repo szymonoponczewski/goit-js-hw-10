209ba02b906a5542a15e77905ae4653bec57d403
Szymon Oponczewski <szymon_o@mac.com>
2023-04-23 14:12:26 +0200

    code update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,77 @@
 import Notiflix from "notiflix";
-const API_URL = "https://restcountries.com/v3.1/name";
+
+const countryEl = document.querySelector(".country-list");
+const countryInfoEl = document.querySelector(".country-info");
+const API_URL = "https://restcountries.com/v3.1/name/";
+const API_FILTER = "?fields=name,capital,population,flags,languages";
+
+const clearFn = () => {
+  countryEl.innerHTML = "";
+  countryInfoEl.innerHTML = "";
+};
+
+const tooManyResults = () => {
+  Notiflix.Notify.info(
+    "Too many matches found. Please enter a more specific name."
+  );
+};
+
+const fewResults = (country) => {
+  clearFn();
+
+  const countries = country
+    .map((countryName) => {
+      return `<li><img src="${countryName.flags.png}" alt="${countryName.flags.alt}" width="25" height="auto"><p> ${countryName.name.common}</p></li>`;
+    })
+    .join(" ");
+
+  countryEl.innerHTML = countries;
+};
+
+const oneResult = (country) => {
+  clearFn();
+
+  const countries = country
+    .map((countryName) => {
+      return `
+       <h2 style="font-size: 35px"><img src="${countryName.flags.png}" alt="${
+        countryName.flags.alt
+      } width="25" height="25"> ${countryName.name.common}</h2>
+        <p><span style="font-weight: bold">Capital:</span> ${
+          countryName.capital
+        }</p>
+        <p><span style="font-weight: bold">Population:</span> ${
+          countryName.population
+        }</p>
+        <p><span style="font-weight: bold">Languages:</span> ${Object.values(
+          countryName.languages
+        ).join(", ")}</p>`;
+    })
+    .join(" ");
+
+  countryInfoEl.innerHTML = countries;
+};
 
 export const fetchCountries = (name) => {
-  return fetch(
-    `${API_URL}/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
+  name = name.trim();
+  fetch(API_URL + name + API_FILTER)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.status);
       }
-      return response.json();
+      return res.json();
     })
-    .catch((error) =>
-      Notiflix.Notify.failure("Oops, there is no country with that name")
-    );
+    .then((country) => {
+      if (country.length > 10) {
+        tooManyResults(country);
+      } else if (country.length >= 2 && country.length <= 10) {
+        fewResults(country);
+      } else if ((country.length = 1)) {
+        oneResult(country);
+      }
+    })
+    .catch(() => {
+      Notiflix.Notify.failure("Oops, there is no country with that name");
+      clearFn();
+    });
 };
--- src/index.js
+++ src/index.js
@@ -1,55 +1,16 @@
 import "./css/styles.css";
 import { fetchCountries } from "./fetchCountries";
-import Notiflix from "notiflix";
 
 const debounce = require("lodash.debounce");
-const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.getElementById("search-box");
-const countryListEl = document.querySelector(".country-list");
-const countryInfo = document.querySelector(".country-info");
+
+const DEBOUNCE_DELAY = 300;
 
 inputEl.addEventListener(
   "input",
-  debounce(async (ev) => {
-    const countryName = ev.target.value.trim();
-
-    if (countryName === "") {
-      countryListEl.innerHTML = "";
-      countryInfo.innerHTML = "";
-      return;
-    }
-
-    const countries = await fetchCountries(countryName);
-
-    if (countries.length > 10) {
-      Notiflix.Notify.info(
-        "Too many matches found. Please enter a more specific name."
-      );
-    } else {
-      countryListEl.innerHTML = countries
-        .map(
-          (country) =>
-            `<li><img height="16" src="${country.flags.png}" />${country.name.common}</li>`
-        )
-        .join("");
-    }
-
-    if (countries.length === 1) {
-      countryInfo.innerHTML = countries
-        .map(
-          (country) => `
-          <p><span style="font-weight: bold">Capital:</span> ${
-            country.capital
-          }</p>
-          <p><span style="font-weight: bold">Population:</span> ${
-            country.population
-          }</p>
-          <p><span style="font-weight: bold">Languages:</span> ${Object.values(
-            country.languages
-          )}</p>`
-        )
-        .join(", ");
-    }
+  debounce((name) => {
+    name = inputEl.value;
+    fetchCountries(name);
   }, DEBOUNCE_DELAY)
 );
